c267d4fe57cabae5f55390609ec1f7a890456c4c
Nekena86 <hei.nekena.6@gmail.com>
2026-08-13 20:44:20 +0300

    feat: add all the security in the project
    * feat: add all the entity
    
    * feat: add all repository
    
    * feat: add exception
    
    * feat: add the package security
    
    * feat: add securityConfig for all the authorization
    
    * fix: resolve the format issue
    
    * fix: add security dependance

---
+++ src/main/java/com/hei/school/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.hei.school.config;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+  private final UserDetailsService userDetailsService;
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public DaoAuthenticationProvider authenticationProvider() {
+    var provider = new DaoAuthenticationProvider();
+    provider.setUserDetailsService(userDetailsService);
+    provider.setPasswordEncoder(passwordEncoder());
+    return provider;
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(
+            auth ->
+                auth.requestMatchers("/promotions/**")
+                    .hasAnyRole("ADMIN", "TEACHER")
+                    .requestMatchers("/api/**")
+                    .authenticated()
+                    .anyRequest()
+                    .authenticated())
+        .httpBasic(basic -> {})
+        .authenticationProvider(authenticationProvider());
+    return http.build();
+  }
+}
